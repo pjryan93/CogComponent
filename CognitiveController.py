2ae7c4788b8c30d8e203e7836c1aaa6ff933021d
Patrick Ryan <pjryan@my.okcu.edu>
2015-04-29 18:22:41 -0500

    changed

--- CognitiveController.py
+++ CognitiveController.py
@@ -39,7 +39,7 @@ def train(datasets):
     start_time = time.clock()
     corruption_levels = [.1,.2,.3]
     pretraining_epochs = 7
-    pretrain_lr = 0.01
+    pretrain_lr = 0.1
     for i in xrange(sda.n_layers):
             # go through pretraining epochs
             for epoch in xrange(pretraining_epochs):
