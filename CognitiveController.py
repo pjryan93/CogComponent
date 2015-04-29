559552191bfe1d58a003cbe7af535cafb04304a0
Patrick Ryan <pjryan@my.okcu.edu>
2015-04-29 18:03:16 -0500

    changed p value

--- CognitiveController.py
+++ CognitiveController.py
@@ -27,7 +27,7 @@ def train(datasets):
             #hidden_layers_sizes=[2300, 2300, 2300,2300,2300],
             #hidden_layers_sizes=[2600, 2500, 2200,2200,2200],
             #hidden_layers_sizes=[2700,2700,2700,2700],
-            hidden_layers_sizes=[800,800],
+            hidden_layers_sizes=[1000,1000,1000,1000],
             n_outs=14
     )
     n_train_batches = train_set_x.get_value(borrow=True).shape[0]
@@ -37,7 +37,7 @@ def train(datasets):
     pretraining_fns = sda.pretraining_functions(train_set_x=train_set_x,batch_size=batch_size)
     print '... pre-training the model'
     start_time = time.clock()
-    corruption_levels = [.07,.07]
+    corruption_levels = [.1,.2,.3,.4]
     pretraining_epochs = 10
     pretrain_lr = 0.001
     for i in xrange(sda.n_layers):
