726eb88ddfd1001fbb3880eeb274ad8476f11352
Patrick Ryan <pjryan@my.okcu.edu>
2015-04-29 23:07:06 -0500

    a

--- CognitiveController.py
+++ CognitiveController.py
@@ -27,7 +27,7 @@ def train(datasets):
             #hidden_layers_sizes=[2300, 2300, 2300,2300,2300],
             #hidden_layers_sizes=[2600, 2500, 2200,2200,2200],
             #hidden_layers_sizes=[2700,2700,2700,2700],
-            hidden_layers_sizes=[1000,1000,1000],
+            hidden_layers_sizes=[1100,1100,1100],
             n_outs=14
     )
     n_train_batches = train_set_x.get_value(borrow=True).shape[0]
